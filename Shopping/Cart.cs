5afeb330a6c726fbd63dcbe7c9856614f03b5571
Mattéo Da Cunha <pl07ccc@eduvaud.ch>
2023-10-13 14:19:26 +0200

    test

--- Shopping/Cart.cs
+++ Shopping/Cart.cs
@@ -12,7 +12,7 @@ namespace Shopping
         #region public methods
         public void Add(List<CartItem> cartItems)
         {
-            throw new NotImplementedException();
+            _cartItems = cartItems;
         }
 
         public void Remove(List<CartItem> cartItemsToRemove)
@@ -24,7 +24,7 @@ namespace Shopping
         {
             get
             {
-                throw new NotImplementedException();
+                return _cartItems;
             }
         }
 
@@ -32,7 +32,12 @@ namespace Shopping
         {
             get
             {
-                throw new NotImplementedException();
+                float currentCartPrice = 0f;
+                foreach (var item in _cartItems)
+                {
+                    currentCartPrice += item.Article.Price * item.Quantity;
+                }
+                return currentCartPrice; ;
             }
         }
         #endregion public methods
